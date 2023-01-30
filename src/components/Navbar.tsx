fb0c8c40a79fe1f13ab9a23e1cfb1b791f580fa1
Gabriel Cornejo <gcornejo441@gmail.com>
2023-01-30 10:42:23 -0800

    feat: updating nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { ReactElement, useState } from 'react';
 
 interface Props {
@@ -48,25 +49,17 @@ function Navbar(props: Props): ReactElement {
         >
           <ul className='mr-auto flex list-none flex-col lg:flex-row'>
             <li className='flex items-center'>
-              <a
+              <Link
                 className={
                   (props.transparent
                     ? 'text-gray-800 lg:text-white lg:hover:text-gray-300'
                     : 'text-gray-800 hover:text-gray-600') +
                   ' flex items-center px-3 py-4 text-xs font-bold uppercase lg:py-2'
                 }
-                href='https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/landing'
+                href='/'
               >
-                <i
-                  className={
-                    (props.transparent
-                      ? 'text-gray-500 lg:text-gray-300'
-                      : 'text-gray-500') +
-                    ' far fa-file-alt leading-lg mr-2 text-lg'
-                  }
-                />{' '}
-                Docs
-              </a>
+                Home
+              </Link>
             </li>
           </ul>
           <ul className='flex list-none flex-col lg:ml-auto lg:flex-row'>
@@ -138,14 +131,15 @@ function Navbar(props: Props): ReactElement {
               <button
                 className={
                   (props.transparent
-                    ? 'bg-white text-gray-800 active:bg-gray-100'
-                    : 'bg-pink-500 text-white active:bg-pink-600') +
-                  ' ml-3 mb-3 rounded px-4 py-2 text-xs font-bold uppercase shadow outline-none hover:shadow-md focus:outline-none lg:mr-1 lg:mb-0'
+                    ? 'bg-yellow-1000 text-gray-800 active:bg-yellow-200'
+                    : 'bg-gray-600 text-white active:bg-gray-700') +
+                  ' m-3 mb-3 rounded px-4 py-2 text-xs font-bold uppercase shadow outline-none hover:shadow-md focus:outline-none lg:mr-1 lg:mb-0'
                 }
                 type='button'
                 style={{ transition: 'all .15s ease' }}
               >
-                <i className='fas fa-arrow-alt-circle-down'></i> Download
+                <i className='fa-solid fa-phone'></i>
+                <span className='ml-2 inline-block lg:hidden'>Contact Us</span>
               </button>
             </li>
           </ul>
